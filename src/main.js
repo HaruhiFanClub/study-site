574060403ba1d74d425e7e7f8c6fbdf4e05dcf83
Suzic <suzichenapk@gmail.com>
2020-06-07 14:22:55 +0800

    登录页面样式

--- src/main.js
+++ src/main.js
@@ -2,12 +2,18 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import { Layout, Avatar } from 'ant-design-vue'
+import VueParticles from 'vue-particles'
+import { Layout, Avatar, Button, Input, Checkbox, Form } from 'ant-design-vue'
 
 Vue.config.productionTip = false
 
+Vue.use(VueParticles)
 Vue.use(Layout)
 Vue.use(Avatar)
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Checkbox)
+Vue.use(Form)
 
 new Vue({
   router,
--- src/views/login.vue
+++ src/views/login.vue
@@ -1,9 +1,27 @@
 <template>
-  <div class="page">
-    <p>页面位置</p>
-    <pre>
-      src-views-login
-    </pre>
+  <div class="login-page">
+    <vue-particles></vue-particles>
+    <div class="login-form">
+      <div class="title">凉宫春日孵化计划</div>
+      <div class="subtitle">共同孵化凉宫圈与应援团的未来</div>
+      <a-form layout="vertical" class="form">
+        <a-form-item>
+          <a-input placeholder="邮箱/QQ" />
+        </a-form-item>
+        <a-form-item>
+          <a-input placeholder="密码" />
+        </a-form-item>
+        <a-form-item>
+          <a-checkbox>记住密码</a-checkbox>
+        </a-form-item>
+        <a-form-item>
+          <a-button type="primary" block>登录</a-button>
+        </a-form-item>
+        <div style="text-align: right;">
+          <a href="#">注册账户</a>
+        </div>
+      </a-form>
+    </div>
   </div>
 </template>
 
@@ -14,5 +32,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+.login-page {
+  width: 100vw;
+  height: 100vh;
+  background-image: url('../assets/images/login-bg.jpg');
+  background-size: cover;
+  background-position: center;
+  position: relative;
+  .login-form {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 390px;
+    height: auto;
+    padding: 20px 10px;
+    background-color: rgba(241, 242, 246, 0.8);
+    border-radius: 5px;
+    transform: translate(-50%, -60%);
+    .title {
+      font-size: 26px;
+      line-height: 28px;
+      font-weight: 650;
+    }
+    .subtitle {
+      margin: 20px 0;
+      font-size: 14px;
+      line-height: 22px;
+      font-weight: 400;
+    }
+    .form {
+      width: 100%;
+    }
+  }
+}
 </style>
