64094308afb1f1a56c5e6b19ba405c4954d901dc
Suzic <suzichenapk@gmail.com>
2020-06-10 07:56:29 +0800

    Fix 路由重载和宽度限制

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <router-view/>
+    <router-view v-if="isRouterAlive" />
   </div>
 </template>
 
@@ -32,7 +32,6 @@ export default {
 html,body{
   width: 100%;
   height: 100%;
-  overflow-x: hidden;
   margin: 0;
   padding: 0;
   font-size: 14px;
