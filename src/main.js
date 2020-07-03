3576787b3cf52ec35add3992723769dddbd324f0
Suzic <suzichenapk@gmail.com>
2020-07-04 06:04:30 +0800

    修改目录结构

--- src/main.js
+++ src/main.js
@@ -3,23 +3,9 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import config from '@/config'
-import VueParticles from 'vue-particles'
-import { ConfigProvider, Layout, Avatar, Button, Input, Checkbox, Form, FormModel, Pagination, Menu, Icon } from 'ant-design-vue'
+import './plugins'
 
 Vue.config.productionTip = false
-
-Vue.use(VueParticles)
-Vue.use(ConfigProvider)
-Vue.use(Icon)
-Vue.use(Layout)
-Vue.use(Avatar)
-Vue.use(Button)
-Vue.use(Input)
-Vue.use(Checkbox)
-Vue.use(Form)
-Vue.use(FormModel)
-Vue.use(Pagination)
-Vue.use(Menu)
 
 /**
  * @description 全局注册应用配置
---
+++ src/plugins/antd.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import { ConfigProvider, Layout, Avatar, Button, Input, Checkbox, Form, FormModel, Pagination, Menu, Icon } from 'ant-design-vue'
+
+Vue.use(ConfigProvider)
+Vue.use(Icon)
+Vue.use(Layout)
+Vue.use(Avatar)
+Vue.use(Button)
+Vue.use(Input)
+Vue.use(Checkbox)
+Vue.use(Form)
+Vue.use(FormModel)
+Vue.use(Pagination)
+Vue.use(Menu)
