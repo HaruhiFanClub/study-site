eef8a6583fd8aeec1323c961687fb91690398464
tdoca <tdoca@outlook.com>
2020-07-12 13:14:30 +0800

    update: 优化页面结构及样式

--- src/layout/Main.vue
+++ src/layout/Main.vue
@@ -2,35 +2,43 @@
   <a-layout class="app-page">
     <a-row>
       <a-layout-header>
-        <div class="logo">
-          <h1>孵化计划</h1>
-        </div>
-        <div class="nav">
-          <router-link to="/">首页</router-link>
-          <router-link to="/study">学习</router-link>
-          <router-link to="/projects">项目</router-link>
-          <router-link to="/source">公共素材</router-link>
-          <router-link to="/">孵化论坛</router-link>
-          <router-link to="/my/index">我的</router-link>
-        </div>
-        <div v-if="userName" class="user-info">
-          <a-avatar class="avatar" :size="32">{{ userName.substr(0, 1) }}</a-avatar>
-          <span class="user-name">{{ userName }}</span>
-          <div class="action-menu-list">
-            <div class="action-menu" @click="logout">退出登录</div>
+        <a-col :xs="0" :sm="1" :md="1" :lg="2" :xl="2" :xxl="2"></a-col>
+        <a-col :xs="24" :sm="22" :md="22" :lg="20" :xl="20" :xxl="20">
+          <div class="page-header">
+            <div class="logo">
+              <h1>孵化计划</h1>
+            </div>
+            <div class="nav">
+              <router-link to="/">首页</router-link>
+              <router-link to="/study">学习</router-link>
+              <router-link to="/projects">项目</router-link>
+              <router-link to="/source">公共素材</router-link>
+              <router-link to="/">孵化论坛</router-link>
+              <router-link to="/my/index">我的</router-link>
+            </div>
+            <div v-if="userName" class="user-info">
+              <a-avatar class="avatar" :size="32">{{ userName.substr(0, 1) }}</a-avatar>
+              <span class="user-name">{{ userName }}</span>
+              <div class="action-menu-list">
+                <div class="action-menu" @click="logout">退出登录</div>
+              </div>
+            </div>
+            <div v-else class="user-info" @click="navToLogin">
+              <span class="user-name">请登录</span>
+            </div>
           </div>
-        </div>
-        <div v-else class="user-info" @click="navToLogin">
-          <span class="user-name">请登录</span>
-        </div>
+        </a-col>
+        <a-col :xs="1" :sm="1" :md="1" :lg="2" :xl="2" :xxl="2"></a-col>
       </a-layout-header>
       <a-layout-content class="main">
-        <div class="page-content">
+        <a-col :xs="0" :sm="0" :md="0" :lg="2" :xl="2" :xxl="2" class="temp"></a-col>
+        <a-col :xs="24" :sm="24" :md="24" :lg="20" :xl="20" :xxl="20" class="page-content">
           <slot name="sider"></slot>
           <div class="page-main">
             <router-view />
           </div>
-        </div>
+        </a-col>
+        <a-col :xs="0" :sm="0" :md="0" :lg="2" :xl="2" :xxl="2" class="temp"></a-col>
       </a-layout-content>
     </a-row>
   </a-layout>
@@ -68,54 +76,91 @@ $page-width: 100%;
   min-height: 100vh;
   margin: 0 auto;
   padding: 0;
-  background-color: #fff;
-  .ant-row {
-    .ant-layout-header {
+  background-color: #f0f2f5;
+  .ant-layout-header {
+    height: $header-height;
+    width: $page-width;
+    margin: 0 auto;
+    padding: 0;
+    color: #a5acb3;
+    background-color: #001529;
+    h1,
+    a {
+      color: #a5acb3;
+    }
+    .page-header {
+      height: $header-height;
+      width: $page-width;
       display: flex;
       flex-wrap: wrap;
       align-items: center;
       justify-content: space-between;
-      height: auto;
-      width: $page-width;
-      margin: 0 auto;
-      padding: 0 10%;
-      color: #a5acb3;
-      background-color: #001529;
-      h1,a {
-        color: #a5acb3;
-      }
+      vertical-align: top;
       .logo {
         cursor: default;
         height: $header-height;
-        display: block;
         h1 {
           display: inline-block;
           height: $header-height;
-          margin: 0;
           line-height: $header-height;
-          font-size: 2rem;
+          margin: 0;
+          font-size: 24px;
           font-weight: 700;
           color: #fff;
         }
       }
       .nav {
-        display: flex;
-        height: 100%;
-        align-items: center;
+        display: inline-flex;
+        height: $header-height;
+        line-height: $header-height;
+        transform: translateX(-100px);
         a {
           display: inline-block;
           text-align: center;
           font-size: 1rem;
-          padding: 0 1rem;
+          padding: 0 2rem;
+        }
+      }
+      @media (max-width: 1100px) {
+        .nav {
+          transform: translateX(-100px);
+          a {
+            padding: 0 1.5rem;
+          }
+        }
+      }
+      @media (max-width: 991px) {
+        .nav {
+          transform: translateX(-100px);
+        }
+      }
+      @media (max-width: 900px) {
+        .nav {
+          transform: translateX(-70px);
+          a {
+            padding: 0 1rem;
+          }
+        }
+      }
+      @media (max-width: 767px) {
+        .nav {
+          transform: translateX(0px);
+        }
+      }
+      @media (max-width: 660px) {
+        .nav {
+          a {
+            padding: 0 0.7rem;
+          }
         }
       }
       .user-info {
         position: relative;
-        display: flex;
+        display: inline-flex;
         align-items: center;
         height: 100%;
         cursor: pointer;
-        .user-name{
+        .user-name {
           font-size: 1rem;
         }
         .avatar {
@@ -151,21 +196,21 @@ $page-width: 100%;
         }
       }
     }
-    .main {
-      width: $page-width;
-      height: auto;
-      min-height: calc(100vh - 64px);
+  }
+  .main {
+    width: $page-width;
+    height: 100%;
+    min-height: calc(100vh - 64px);
+    margin: 0 auto;
+    padding-top: 22px;
+    // background-color: #f0f2f5;
+    .page-content {
+      display: flex;
+      // width: 100%;
       margin: 0 auto;
-      padding-top: 22px;
-      background-color: #f0f2f5;
-      .page-content {
-        display: flex;
-        width: 100%;
-        margin: 0 auto;
-        background-color: #fff;
-        .page-main {
-          flex: 1;
-        }
+      background-color: #fff;
+      .page-main {
+        flex: 1;
       }
     }
   }
--- src/views/main/home.vue
+++ src/views/main/home.vue
@@ -1,111 +1,106 @@
 <template>
-  <div class="home-page">
-    <a-row>
-      <a-col :xs="12" :sm="14" :md="16" :lg="17" :xl="18">
-        <div class="main-side">
-          <section class="learning-list-container">
-            <div class="list">
-              <div class="item" v-for="item in learningList" :key="item.id">
-                <a-avatar shape="square" alt="用户头像" :size="40" :src="item.avatar" />
-                <div class="info">
-                  <div class="user-name">{{ item.userName }}</div>
-                  <div class="desc">
-                    <span>正在进行 </span>
-                    <router-link to="/">{{ item.studyTitle }}</router-link>
-                    <span> 的学习</span>
-                  </div>
-                </div>
-                  <a-progress :percent="item.percent" size="small" style="width: 200px;" />
+  <a-row class="home-page">
+    <a-col :xs="24" :sm="24" :md="18" :lg="18" :xl="18" :xxl="18" class="main-side">
+      <section class="learning-list-container">
+        <div class="list">
+          <div class="item" v-for="item in learningList" :key="item.id">
+            <a-avatar shape="square" alt="用户头像" :size="40" :src="item.avatar" />
+            <div class="info">
+              <div class="user-name">{{ item.userName }}</div>
+              <div class="desc">
+                <span>正在进行</span>
+                <router-link to="/">{{ item.studyTitle }}</router-link>
+                <span>的学习</span>
               </div>
             </div>
-            <div class="action">
-              <router-link to="/study">
-                <a-button size="large">了解详情</a-button>
-              </router-link>
-            </div>
-          </section>
-          <section class="project-list-container">
-            <project-list :data="projectList"></project-list>
-            <div class="action">
-              <router-link to="/">
-                <a-button size="large" style="margin-right: 20px;">我想立项</a-button>
-              </router-link>
-              <router-link to="/projects">
-                <a-button size="large">查看更多</a-button>
-              </router-link>
-            </div>
-          </section>
-          <section class="source-list-container">
-            <source-list :data="sourceList"></source-list>
-            <div class="action">
-              <router-link to="/source">
-                <a-button size="large">查看更多</a-button>
-              </router-link>
-            </div>
-          </section>
+            <a-progress :percent="item.percent" size="small" style="width: 200px;" />
+          </div>
         </div>
-      </a-col>
-      <a-col :xs="12" :sm="10" :md="8" :lg="7" :xl="6">
-        <div class="right-side">
-          <section class="about">
-            <div class="desc">
-              "我们希望用这个企划，<br>
-              孵化凉宫圈的未来"<br>
-            </div>
-            <a-button class="about-btn">点击这里，了解详情</a-button>
-          </section>
-          <section class="user-info">
-            <div class="user-name">{{ userName }}</div>
-            <div class="item-info">
-              <label>积分：</label>
-              <span class="value">121</span>
-            </div>
-            <div class="item-info">
-              <label>总积分：</label>
-              <span class="value">422</span>
-            </div>
-            <div class="item-info">
-              <label>参与学习：</label>
-              <span class="value">3</span>
-            </div>
-            <div class="item-info">
-              <label>完成学习：</label>
-              <span class="value">2</span>
-            </div>
-            <div class="item-info">
-              <label>创建项目：</label>
-              <span class="value">1</span>
-            </div>
-            <div class="item-info">
-              <label>参与项目：</label>
-              <span class="value">3</span>
-            </div>
-            <div class="item-info">
-              <label>投稿素材：</label>
-              <span class="value">0</span>
-            </div>
-          </section>
-          <section class="bbs">
-            <div class="item">
-              <div class="title">讨论</div>
-              <p>帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题</p>
-            </div>
-            <div class="item">
-              <div class="title">经验</div>
-              <p>帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题</p>
-            </div>
-            <div class="item">
-              <div class="title">展示</div>
-              <p>帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题</p>
-            </div>
-            <div class="action">
-              <router-link to="/">进入论坛</router-link>
-            </div>
-          </section>
+        <div class="action">
+          <router-link to="/study">
+            <a-button size="large">了解详情</a-button>
+          </router-link>
         </div>
-      </a-col>
-    </a-row>
-  </div>
+      </section>
+      <section class="project-list-container">
+        <project-list :data="projectList"></project-list>
+        <div class="action">
+          <router-link to="/">
+            <a-button size="large" style="margin-right: 20px;">我想立项</a-button>
+          </router-link>
+          <router-link to="/projects">
+            <a-button size="large">查看更多</a-button>
+          </router-link>
+        </div>
+      </section>
+      <section class="source-list-container">
+        <source-list :data="sourceList"></source-list>
+        <div class="action">
+          <router-link to="/source">
+            <a-button size="large">查看更多</a-button>
+          </router-link>
+        </div>
+      </section>
+    </a-col>
+    <a-col :xs="0" :sm="0" :md="6" :lg="6" :xl="6" :xxl="6" class="right-side">
+      <section class="about">
+        <div class="desc">
+          "我们希望用这个企划，
+          <br />孵化凉宫圈的未来"
+          <br />
+        </div>
+        <a-button class="about-btn">点击这里，了解详情</a-button>
+      </section>
+      <section class="user-info">
+        <div class="user-name">{{ userName }}</div>
+        <div class="item-info">
+          <label>积分：</label>
+          <span class="value">121</span>
+        </div>
+        <div class="item-info">
+          <label>总积分：</label>
+          <span class="value">422</span>
+        </div>
+        <div class="item-info">
+          <label>参与学习：</label>
+          <span class="value">3</span>
+        </div>
+        <div class="item-info">
+          <label>完成学习：</label>
+          <span class="value">2</span>
+        </div>
+        <div class="item-info">
+          <label>创建项目：</label>
+          <span class="value">1</span>
+        </div>
+        <div class="item-info">
+          <label>参与项目：</label>
+          <span class="value">3</span>
+        </div>
+        <div class="item-info">
+          <label>投稿素材：</label>
+          <span class="value">0</span>
+        </div>
+      </section>
+      <section class="bbs">
+        <div class="item">
+          <div class="title">讨论</div>
+          <p>帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题</p>
+        </div>
+        <div class="item">
+          <div class="title">经验</div>
+          <p>帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题</p>
+        </div>
+        <div class="item">
+          <div class="title">展示</div>
+          <p>帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题帖子标题</p>
+        </div>
+        <div class="action">
+          <router-link to="/">进入论坛</router-link>
+        </div>
+      </section>
+    </a-col>
+  </a-row>
 </template>
 
 <script>
@@ -116,28 +111,32 @@ const mockData = {
     {
       id: '1',
       userName: '电脑社成员',
-      avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
+      avatar:
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
       studyTitle: '学习方向-子方向-学习等级',
       percent: 60
     },
     {
       id: '2',
       userName: '电脑社成员',
-      avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
+      avatar:
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
       studyTitle: '学习方向-子方向-学习等级',
       percent: 60
     },
     {
       id: '3',
       userName: '电脑社成员',
-      avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
+      avatar:
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
       studyTitle: '学习方向-子方向-学习等级',
       percent: 60
     },
     {
       id: '4',
       userName: '电脑社成员',
-      avatar: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
+      avatar:
+        'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1593935723191&di=f95de4d1d1eddc7ac1061cdfdaee2cef&imgtype=0&src=http%3A%2F%2Fimg.duoziwang.com%2F2016%2F11%2F23%2F16432937557.jpg',
       studyTitle: '学习方向-子方向-学习等级',
       percent: 60
     }
@@ -146,10 +145,9 @@ const mockData = {
     {
       id: '1',
       title: '项目名称项目名称项目名称',
-      tags: [
-        '游戏', 'RPG Maker', '像素风'
-      ],
-      desc: '项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介',
+      tags: ['游戏', 'RPG Maker', '像素风'],
+      desc:
+        '项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介',
       progress: '招募中',
       addDate: '2017-10-01 18:00',
       authorInfo: {
@@ -161,10 +159,9 @@ const mockData = {
     {
       id: '2',
       title: '项目名称项目名称项目名称',
-      tags: [
-        '游戏', 'RPG Maker', '像素风'
-      ],
-      desc: '项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介',
+      tags: ['游戏', 'RPG Maker', '像素风'],
+      desc:
+        '项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介项目简介',
       progress: '招募中',
       addDate: '2017-10-01 18:00',
       authorInfo: {
@@ -178,10 +175,9 @@ const mockData = {
     {
       id: '1',
       title: '素材名称素材名称素材名称',
-      tags: [
-        '游戏', 'RPG Maker', '像素风'
-      ],
-      desc: '素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介',
+      tags: ['游戏', 'RPG Maker', '像素风'],
+      desc:
+        '素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介',
       addDate: '2017-10-01 18:00',
       authorInfo: {
         userId: '1',
@@ -192,10 +188,9 @@ const mockData = {
     {
       id: '2',
       title: '素材名称素材名称素材名称',
-      tags: [
-        '游戏', 'RPG Maker', '像素风'
-      ],
-      desc: '素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介',
+      tags: ['游戏', 'RPG Maker', '像素风'],
+      desc:
+        '素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介素材简介',
       addDate: '2017-10-01 18:00',
       authorInfo: {
         userId: '1',
@@ -229,86 +224,83 @@ export default {
 <style lang="scss" scoped>
 .home-page {
   display: flex;
-  padding: 30px 30px 20px 0;
-  .ant-row{
-    .main-side {
-      flex: 1;
-      margin-right: 30px;
-      section {
-        padding: 20px;
-        border-bottom: 1px solid #e9e9e9;
-        .action {
+  padding: 32px;
+  .main-side {
+    flex: 1;
+    margin-right: 30px;
+    section {
+      padding: 20px;
+      border-bottom: 1px solid #e9e9e9;
+      .action {
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding-top: 20px;
+      }
+    }
+    .learning-list-container {
+      padding: 0 0 20px 0;
+      .list {
+        .item {
           display: flex;
           align-items: center;
-          justify-content: flex-end;
-          padding-top: 20px;
-        }
-      }
-      .learning-list-container {
-        padding: 0 0 20px 0;
-        .list {
-          .item {
-            display: flex;
-            align-items: center;
-            padding: 20px;
-            border-bottom: 1px solid #e9e9e9;
-            .info {
-              flex: 1;
-              margin-left: 20px;
-            }
+          padding: 20px;
+          border-bottom: 1px solid #e9e9e9;
+          .info {
+            flex: 1;
+            margin-left: 20px;
           }
         }
       }
     }
-    .right-side {
-      width: 264px;
-      section {
-        padding: 15px 15px 10px 15px;
+  }
+  .right-side {
+    section {
+      padding: 15px 15px 10px 15px;
+      margin-bottom: 10px;
+      border: 1px solid #e9e9e9;
+      &:last-child {
+        margin-bottom: 0;
+      }
+    }
+    .about {
+      width: 100%;
+      text-align: center;
+      .desc {
         margin-bottom: 10px;
-        border: 1px solid #e9e9e9;
-        &:last-child {
-          margin-bottom: 0;
+        font-size: 18px;
+        font-weight: 700;
+      }
+    }
+    .user-info {
+      .user-name {
+        font-size: 16px;
+        color: rgba(0, 0, 0, 0.85);
+      }
+      .item-info {
+        line-height: 2.5em;
+        .value {
+          color: rgba(0, 0, 0, 0.43);
         }
       }
-      .about {
-        width: 100%;
-        text-align: center;
-        .desc {
-          margin-bottom: 10px;
-          font-size: 18px;
-          font-weight: 700;
-        }
-      }
-      .user-info {
-        .user-name {
+    }
+    .bbs {
+      .item {
+        .title {
+          margin-bottom: 5px;
           font-size: 16px;
+          font-weight: 700;
           color: rgba(0, 0, 0, 0.85);
         }
-        .item-info {
-          line-height: 2.5em;
-          .value {
-            color: rgba(0, 0, 0, 0.43);
-          }
-        }
-      }
-      .bbs {
-        .item {
-          .title {
-            margin-bottom: 5px;
-            font-size: 16px;
-            font-weight: 700;
-            color: rgba(0, 0, 0, 0.85);
-          }
-          p {
-            $line-height: 20px;
-            $max-line: 3;
-            display: -webkit-box;
-            -webkit-box-orient: vertical;
-            -webkit-line-clamp: $max-line;
-            text-overflow: ellipsis;
-            height: $line-height * $max-line;
-            overflow: hidden;
-          }
+        p {
+          $line-height: 20px;
+          $max-line: 3;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: $max-line;
+          text-overflow: ellipsis;
+          height: $line-height * $max-line;
+          overflow: hidden;
         }
       }
     }
